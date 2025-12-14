42b47f70780715cb6713d6e7b60a2d8c0398ad9f
guswo1118 <guswo1118@naver.com>
2025-12-15 04:17:03 +0900

    feat : 1일차 프로젝트생성 및 MySQL 연동 완료

---
+++ src/main/java/io/github/hyunjaegit/aws_secutiry/AwsSecutiryApplication.java
@@ -0,0 +1,13 @@
+package io.github.hyunjaegit.aws_secutiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSecutiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSecutiryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/hyunjaegit/aws_secutiry/AwsSecutiryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hyunjaegit.aws_secutiry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSecutiryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
